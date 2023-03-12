a28e1e55f0a212fdc81b271418e5792446877eb0
guruadp <gurunandhanadp@gmail.com>
2023-03-12 17:48:44 -0400

    get start and goal node inputs

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -73,5 +73,30 @@ def create_obstacle_map():
     # plt.show()
     return obstacle_points
 
-obs_points = create_obstacle_map()
-print(obs_points)
+def get_input():
+    accept_start_node, accept_goal_node = True, True
+    while accept_start_node:
+        start_x = int(input("Enter start x: "))
+        start_y = int(input("Enter start y: "))
+        start_node = (start_x, start_y)
+        if start_node not in obs_points:
+            accept_start_node = False
+            # print("Valid nodes")
+        else:
+            print("You have entered invalid start node. Please enter a valid note...")
+            continue
+
+    while accept_goal_node:    
+        goal_x = int(input("Enter goal x: "))
+        goal_y = int(input("Enter goal y: "))        
+        goal_node = (goal_x, goal_y)
+        if goal_node not in obs_points:
+            accept_goal_node = False
+            print("Valid nodes")
+        else:
+            print("You have entered invalid goal node. Please enter a valid note...")
+    return start_node, goal_node
+# obs_points = create_obstacle_map()
+# print(obs_points)
+obs_points = [(50, 50), (100, 100)]
+start_node, goal_node = get_input()
